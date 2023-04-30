8a5467509145e0294a53a4cae742b8e1500a1390
poggerr <etor0pov@yandex.ru>
2023-04-30 18:30:24 +0300

    added location

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,47 +1,74 @@
 package main
 
 import (
+	"flag"
 	"github.com/gorilla/mux"
 	"github.com/poggerr/go_shortener/internal/app"
 	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	if err := run(); err != nil {
-		panic(err)
-	}
-}
+	//if err := run(); err != nil {
+	//	panic(err)
+	//}
 
-func run() error {
+	addr := flag.String("addr", ":8080", "Сетевой адрес веб-сервера")
+	flag.Parse()
+
+	// Используйте log.New() для создания логгера для записи информационных сообщений. Для этого нужно
+	// три параметра: место назначения для записи логов (os.Stdout), строка
+	// с префиксом сообщения (INFO или ERROR) и флаги, указывающие, какая
+	// дополнительная информация будет добавлена. Обратите внимание, что флаги
+	// соединяются с помощью оператора OR |.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Создаем логгер для записи сообщений об ошибках таким же образом, но используем stderr как
+	// место для записи и используем флаг log.Lshortfile для включения в лог
+	// названия файла и номера строки где обнаружилась ошибка.
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	router := mux.NewRouter()
 	router.HandleFunc(`/`, postPage)
 	router.HandleFunc(`/{id}`, getPage)
-	return http.ListenAndServe(`:8080`, router)
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  router,
+	}
+
+	infoLog.Printf("Запуск сервера на %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
+
+}
+
+func run() {
+
+	//return http.ListenAndServe(`:8080`, router)
 }
 
 func getPage(res http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	id := vars["id"]
 	if req.Method != http.MethodGet {
-		http.Error(res, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
+		http.Error(res, "Only GET requests are allowed!", http.StatusBadRequest)
 		return
 	}
 
 	ans := app.UnShorting(id)
 
-	res.Header().Set("content-type", "text/plain")
+	res.Header().Set("Location", ans)
+	res.WriteHeader(307)
 
-	res.WriteHeader(http.StatusTemporaryRedirect)
-
-	res.Header().Set("Location", "https://practicum.yandex.ru/ ")
-
-	res.Write([]byte(ans))
 }
 
 func postPage(res http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
-		http.Error(res, "Only Post requests are allowed!", http.StatusMethodNotAllowed)
+		http.Error(res, "Only Post requests are allowed!", http.StatusBadRequest)
 		return
 	}
 
