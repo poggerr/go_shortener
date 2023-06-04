f467d1405d0cae7de8cebe97c86cc9c3454c3cf7
poggerr <etor0pov@yandex.ru>
2023-06-04 21:00:42 +0300

    расследование.....8

--- internal/config/config.go
+++ internal/config/config.go
@@ -20,27 +20,8 @@ func NewConf() *Config {
 
 	flag.StringVar(&cfg.Serv, "a", ":8080", "write down server")
 	flag.StringVar(&cfg.DefUrl, "b", "http://localhost:8080", "write down default url")
-	flag.StringVar(&cfg.Path, "f", "", "write down path to storage")
+	flag.StringVar(&cfg.Path, "f", "/tmp/short-url-db.json", "write down path to storage")
 	flag.Parse()
-
-	//dir, _ := path.Split(cfg.Path)
-	//if _, err := os.Stat(dir); os.IsNotExist(err) {
-	//	err = os.Mkdir(dir, 0666)
-	//	if err != nil {
-	//		logger.Initialize().Info(err)
-	//	}
-	//}
-	//
-	//file, err := os.OpenFile(cfg.Path, os.O_RDONLY|os.O_CREATE, 0666)
-	//defer func(file *os.File) {
-	//	err = file.Close()
-	//	if err != nil {
-	//		logger.Log.Error(err)
-	//	}
-	//}(file)
-	//if err != nil {
-	//	logger.Log.Error(err)
-	//}
 
 	return &cfg
 }
