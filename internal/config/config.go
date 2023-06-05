119cc8fc8f0a32e7eb3465a87e873a71b68e3059
poggerr <etor0pov@yandex.ru>
2023-06-05 19:56:43 +0300

    поправил флаги v6

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,7 +21,8 @@ func NewConf() *Config {
 	if cfg.Serv == "" || cfg.DefUrl == "" || cfg.Path == "" {
 		flag.StringVar(&cfg.Serv, "a", ":8080", "write down server")
 		flag.StringVar(&cfg.DefUrl, "b", "http://localhost:8080", "write down default url")
-		//flag.StringVar(&cfg.Path, "f", "/tmp/short-url-db.json", "write down path to storage")
+		flag.StringVar(&cfg.Path, "f", "/tmp/short-url-db.json", "write down path to storage")
+		fmt.Println(cfg.Path)
 		flag.Parse()
 	}
 
